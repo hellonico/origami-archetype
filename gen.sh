29f8cb27e6f8f5fced0932b67e701cd025df8293
Nicolas Modrzyk <hellonico@gmail.com>
2020-05-25 18:10:26 +0900

    script

---
+++ gen.sh
@@ -0,0 +1,8 @@
+mvn org.apache.maven.plugins:maven-archetype-plugin:2.4:generate \
+-DarchetypeArtifactId=maven-archetype \
+-DarchetypeGroupId=origami \
+-DarchetypeVersion=4.3 \
+-DarchetypeCatalog=https://repository.hellonico.info/repository/hellonico/ \
+-Dversion=1.0-SNAPSHOT \
+-DgroupId=hello \
+-DartifactId=cvj
