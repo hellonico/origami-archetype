e0d9e8d1defc451f3b715d7d18e8c29019f44931
Nicolas Modrzyk <hellonico@gmail.com>
2023-06-15 17:54:48 +0900

    update template

--- src/main/resources/archetype-resources/src/main/java/YouTube.java
+++ src/main/resources/archetype-resources/src/main/java/YouTube.java
@@ -3,15 +3,17 @@ package hello;
 import origami.Camera;
 import origami.Filter;
 import origami.Origami;
-import origami.filters.Grayscale;
+import origami.filters.detect.yolo.MyYolo;
 
 public class YouTube {
 
+
     public static void main(String[] args) throws InterruptedException, ClassNotFoundException {
+
         Origami.init();
         Class.forName(origami.video.YouTubeHandler.class.getName());
-        final String input = args.length > 0 ? args[0] : "youtube://DXUAyRRkI6k";
-        Filter f = new Grayscale();
+        final String input = args.length > 0 ? args[0] : "{:device \"youtube://DXUAyRRkI6k\" :slow 100}";
+        Filter f = new MyYolo.V3Tiny();
         new Camera().device(input).filter(f).run();
     }
 
